dbabdb54ef498cd5c550db684244efc9d640c998
Debabrata Choudhury <sudodeba97.iiitbbsr@gmail.com>
2024-02-10 18:16:18 +0530

    y2015 day3 part2 completed

--- y2015/d3/src/part2.rs
+++ y2015/d3/src/part2.rs
@@ -1,22 +1,87 @@
+use std::collections::HashSet;
 use std::fs;
 #[allow(dead_code, unused, non_snake_case)]
 pub fn solution(input_path: String) {
     let lines = fs::read_to_string(input_path).unwrap();
-    let mut current_floor_value = 0;
-    let mut num_list = 0;
+    let mut santa_houses_hashset = HashSet::from([(0, 0)]);
+    let mut robo_santa_house_set = HashSet::from([(0, 0)]);
 
-    // TODO: Need to find a functional way to do it
-    for (i, c) in lines.chars().enumerate() {
-        current_floor_value = match c {
-            '(' => current_floor_value + 1,
-            ')' => current_floor_value - 1,
-            _ => current_floor_value,
-        };
-        if current_floor_value == -1 {
-            num_list = i + 1;
-            break;
+    let mut santa_current_position: (i32, i32) = (0, 0);
+    let mut robo_santa_current_position: (i32, i32) = (0, 0);
+
+    lines.chars().enumerate().for_each(|(i, c)| match c {
+        '>' => {
+            if i % 2 != 0 {
+                santa_houses_hashset
+                    .insert((santa_current_position.0 + 1, santa_current_position.1));
+                santa_current_position = (santa_current_position.0 + 1, santa_current_position.1);
+            } else {
+                robo_santa_house_set.insert((
+                    robo_santa_current_position.0 + 1,
+                    robo_santa_current_position.1,
+                ));
+                robo_santa_current_position = (
+                    robo_santa_current_position.0 + 1,
+                    robo_santa_current_position.1,
+                );
+            }
         }
-    }
+        '<' => {
+            if i % 2 != 0 {
+                santa_houses_hashset
+                    .insert((santa_current_position.0 - 1, santa_current_position.1));
+                santa_current_position = (santa_current_position.0 - 1, santa_current_position.1);
+            } else {
+                robo_santa_house_set.insert((
+                    robo_santa_current_position.0 - 1,
+                    robo_santa_current_position.1,
+                ));
+                robo_santa_current_position = (
+                    robo_santa_current_position.0 - 1,
+                    robo_santa_current_position.1,
+                );
+            }
+        }
+        '^' => {
+            if i % 2 != 0 {
+                santa_houses_hashset
+                    .insert((santa_current_position.0, santa_current_position.1 + 1));
+                santa_current_position = (santa_current_position.0, santa_current_position.1 + 1);
+            } else {
+                robo_santa_house_set.insert((
+                    robo_santa_current_position.0,
+                    robo_santa_current_position.1 + 1,
+                ));
+                robo_santa_current_position = (
+                    robo_santa_current_position.0,
+                    robo_santa_current_position.1 + 1,
+                );
+            }
+        }
+        'v' => {
+            if i % 2 != 0 {
+                santa_houses_hashset
+                    .insert((santa_current_position.0, santa_current_position.1 - 1));
+                santa_current_position = (santa_current_position.0, santa_current_position.1 - 1);
+            } else {
+                robo_santa_house_set.insert((
+                    robo_santa_current_position.0,
+                    robo_santa_current_position.1 - 1,
+                ));
+                robo_santa_current_position = (
+                    robo_santa_current_position.0,
+                    robo_santa_current_position.1 - 1,
+                );
+            }
+        }
+        _ => (),
+    });
 
-    println!("Day 3 Part2 solution: {}", num_list);
+    println!("{:?}", santa_houses_hashset);
+    println!("{:?}", robo_santa_house_set);
+
+    println!(
+        "Day 3 Part2 solution: {}",
+        santa_houses_hashset.union(&robo_santa_house_set).count()
+    );
 }
